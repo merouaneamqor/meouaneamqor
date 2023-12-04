0357d207a5e9f2a3ce4f5dfae0f7b1cb759d1937
AMQOR Merouane <marouane.amqor6@gmail.com>
2023-12-04 17:40:08 +0100

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  vite: {
+    server: {
+        fs: {
+            allow: [".."]
+        }
+    }
+  },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ]
+})
