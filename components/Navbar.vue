2d3efbc5510f61fb32f332794a07685fb3656df5
AMQOR Merouane <marouane.amqor6@gmail.com>
2023-12-06 23:33:04 +0100

    update header

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,13 +1,80 @@
 <template>
-  <header class="bg-gray-800 text-white body-font">
-    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-        <a href="/" class="mr-5 hover:text-gray-400">Home</a>
-        <a href="/about" class="mr-5 hover:text-gray-400">About</a>
-        <a href="/portfolio" class="mr-5 hover:text-gray-400">Portfolio</a>
-        <a href="/contact" class="mr-5 hover:text-gray-400">Contact</a> 
-      </nav>
-      <button class="inline-flex items-center bg-gray-700 border-0 py-1 px-3 focus:outline-none hover:bg-gray-600 rounded text-base mt-4 md:mt-0">Button</button>
+  <div class="relative bg-gray-800">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6">
+      <div class="flex justify-between items-center border-b-2 border-gray-800 py-6 md:justify-start md:space-x-10">
+        <!-- Logo Section -->
+        <div class="flex justify-start lg:w-0 lg:flex-1 text-white">
+          <a href="#">
+            <h1>AMQOR Merouane</h1>
+          </a>
+        </div>
+
+        <!-- Hamburger Menu for Mobile -->
+        <div class="md:hidden">
+          <button @click="isMenuOpen = !isMenuOpen" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100">
+            <span class="sr-only">Open menu</span>
+            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m4 6H4"></path>
+            </svg>
+          </button>
+        </div>
+
+        <!-- Desktop Navigation -->
+        <nav class="hidden md:flex space-x-10">
+          <a v-for="link in links" :key="link.href" :href="link.href" class="text-base font-medium text-gray-100 hover:text-gray-200">{{ link.text }}</a>
+        </nav>
+
+      </div>
     </div>
-  </header>
+
+    <!-- Mobile Menu -->
+    <div v-if="isMenuOpen" class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+      <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+        <div class="pt-5 pb-6 px-5">
+          <div class="flex items-center justify-between">
+            <div>
+              <!-- Mobile Logo -->
+              <h1>AMQOR Merouane</h1>
+              
+            </div>
+            <!-- Close Mobile Menu Button -->
+            <div class="-mr-2">
+              <button @click="isMenuOpen = false" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100">
+                <span class="sr-only">Close menu</span>
+                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+          <!-- Mobile Navigation Links -->
+          <nav class="mt-6">
+            <a v-for="link in links" :key="link.href" :href="link.href" class="block p-3 rounded-md hover:bg-gray-50">{{ link.text }}</a>
+
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isMenuOpen: false,
+      links: [
+        { href: 'https://github.com/merouaneamqor', text: 'GitHub' },
+        { href: 'https://linkedin.com/in/merouane-amqor', text: 'LinkedIn' },
+        // Add more links as needed
+      ]
+    };
+  },
+  // Other script code
+};
+</script>
+
+
+<style scoped>
+/* Your CSS goes here */
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -18,16 +18,10 @@
 <script setup lang="ts">
 
 useHead({
-  title: 'Home - Merouane Amqor',
-  meta: [
-    { name: 'description', content: 'Welcome to the home page of Merouane Amqor, a Ruby on Rails expert in Casablanca.' }
-    // Other page-specific meta tags
-  ],
-  // Other head attributes
+
 });
 
 useSeoMeta({
-  title: 'Home - Merouane Amqor',
   ogTitle: 'Home - Merouane Amqor',
   ogDescription: 'Discover the portfolio and expertise of Merouane Amqor in web development.',
   ogImage: 'https://avatars.githubusercontent.com/u/37594453?v=4',
