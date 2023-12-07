a5d7561ea2e7c1893746c407d7e554763eb9f534
AMQOR Merouane <marouane.amqor6@gmail.com>
2023-12-07 10:36:41 +0100

    Update json ld

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,74 +12,40 @@
 <script>
 export default {
   jsonld() {
-    return        {
-  "@context": "https://schema.org",
-  "@type": "Person",
-  "name": "Merouane AMQOR",
-  "image": "https://avatars.githubusercontent.com/u/37594453?v=4",
-  "jobTitle": "Ruby on Rails Developer | Certified ScrumMaster®",
-  "email": "marouane.amqor@gmail.com",
-  "telephone": "+212701086726",
-  "url": "http://linkedin.com/in/merouane-amqor",
-  "sameAs": "http://linkedin.com/in/merouane-amqor",
-  "knowsLanguage": ["French", "English", "Arabic"],
-  "address": {
-    "@type": "PostalAddress",
-    "addressLocality": "Casablanca, Morocco"
-  },
-  "alumniOf": {
-    "@type": "EducationalOrganization",
-    "name": "SUPEMIR, Casablanca, Morocco"
-  },
-  "memberOf": {
-    "@type": "Organization",
-    "name": "Scrum Alliance"
-  },
-  "worksFor": [
-    {
-      "@type": "Organization",
-      "name": "DabaDoc",
-      "description": "Full-stack Developer",
-      "startDate": "2018-09",
-      "endDate": "2022-04"
-    },
-    {
-      "@type": "Organization",
-      "name": "Meram / Texam",
-      "description": "Co-founder",
-      "startDate": "2018",
-      "endDate": "2019"
-    },
-    {
-      "@type": "Organization",
-      "name": "Riad Raha",
-      "description": "Information System Developer",
-      "startDate": "2021",
-      "endDate": "2021"
-    },
-    {
-      "@type": "Organization",
-      "name": "Majorel",
-      "description": "Back-end Developer"
-    },
-    {
-      "@type": "Organization",
-      "name": "Beyond Expertise",
-      "description": "Front-end / Back-end Developer"
-    }
-  ],
-  "hasCredential": [
-    {
-      "@type": "EducationalOccupationalCredential",
-      "credentialCategory": "Certified ScrumMaster®",
-      "recognizedBy": {
-        "@type": "Organization",
-        "name": "Scrum Alliance"
+    return {
+      "@context": "https://schema.org",
+      "@type": "ProfilePage",
+      "mainEntity": {
+        "@type": "Person",
+        "name": "Merouane AMQOR",
+        "jobTitle": "Ruby on Rails Developer | Certified ScrumMaster®",
+        "url": "http://merouaneamqor.com",
+        "email": "marouane.amqor@gmail.com",
+        "telephone": "+212701086726",
+        "address": {
+          "@type": "PostalAddress",
+          "addressLocality": "Casablanca, Morocco"
+        },
+        "image": "https://avatars.githubusercontent.com/u/37594453?v=4",
+        "sameAs": [
+          "https://linkedin.com/in/merouane-amqor",
+          "https://instagram.com/merouaneamqor"
+        ],
+        "interactionStatistic": [
+          {
+            "@type": "InteractionCounter",
+            "interactionType": "https://schema.org/FollowAction",
+            "userInteractionCount": 0 
+          },
+          {
+            "@type": "InteractionCounter",
+            "interactionType": "https://schema.org/LikeAction",
+            "userInteractionCount": 0
+          }
+        ],
+        "description": "Experienced Software Engineer with a strong focus on Ruby on Rails. Skilled in a variety of platforms and languages, with a particular proficiency in back-end development. Strong leadership skills and experienced in leading teams."
       }
     }
-  ]
-}
-
-  },
+  }
 }
 </script>
