e55373eed11018b6243ae4fbc6237af43dcad4f9
Merouane AMQOR <marouane.amqor6@gmail.com>
2023-12-21 12:53:09 +0100

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -39,6 +39,7 @@ export default {
           "https://instagram.com/merouaneamqor",
           "https://rubygems.org/profiles/merouaneamqor",
           "https://github.com/merouaneamqor",
+          "https://www.crunchbase.com/person/amqor-merouane",
         ],
         "interactionStatistic": [
           {
